62ae448c69e4c31a73dff080b14bc2ff60c1156e
thuanhoang-tsp <thuan.hoang@tsp.com.vn>
2024-12-03 11:33:27 +0700

    Setup JSP

---
+++ src/main/java/com/example/java_spring_mvc/config/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package com.example.java_spring_mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Bean
+    public ViewResolver viewResolver() {
+        final InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(viewResolver());
+    }
+}
--- src/main/java/com/example/java_spring_mvc/controller/UserController.java
+++ src/main/java/com/example/java_spring_mvc/controller/UserController.java
@@ -1,20 +1,20 @@
 package com.example.java_spring_mvc.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.java_spring_mvc.service.UserService;
 
-@RestController
+@Controller
 public class UserController {
-    private UserService userService;
+    // private UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    // public UserController(UserService userService) {
+    // this.userService = userService;
+    // }
 
     @GetMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        return "hello";
     }
 }
