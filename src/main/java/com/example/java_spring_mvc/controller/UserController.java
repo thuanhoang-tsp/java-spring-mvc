f38c59b8db1ebcdc90adb55d9e671c79a58cb83e
thuanhoang-tsp <thuan.hoang@tsp.com.vn>
2024-12-03 14:05:57 +0700

    JSTL trong View

--- src/main/java/com/example/java_spring_mvc/controller/UserController.java
+++ src/main/java/com/example/java_spring_mvc/controller/UserController.java
@@ -1,20 +1,24 @@
 package com.example.java_spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.java_spring_mvc.service.UserService;
 
 @Controller
 public class UserController {
-    // private UserService userService;
+    private UserService userService;
 
-    // public UserController(UserService userService) {
-    // this.userService = userService;
-    // }
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        String test = userService.handleHello();
+        model.addAttribute("thuan", test);
+        model.addAttribute("thuan111", "hjhj");
         return "hello";
     }
 }
