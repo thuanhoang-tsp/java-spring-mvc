326d597bebd5c425c50758f43c09b2d873a44339
thuanhoang-tsp <thuan.hoang@tsp.com.vn>
2024-12-04 11:31:22 +0700

    init UI

--- src/main/java/com/example/java_spring_mvc/controller/UserController.java
+++ src/main/java/com/example/java_spring_mvc/controller/UserController.java
@@ -3,8 +3,15 @@ package com.example.java_spring_mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.java_spring_mvc.domain.User;
 import com.example.java_spring_mvc.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
@@ -19,6 +26,18 @@ public class UserController {
         String test = userService.handleHello();
         model.addAttribute("thuan", test);
         model.addAttribute("thuan111", "hjhj");
-        return "hello";
+        return "home";
+    }
+
+    @GetMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("user", new User());
+        return "/admin/user/create";
+    }
+
+    @PostMapping("/admin/user/create1")
+    public String createUserPage(@ModelAttribute("user") User user) {
+        System.out.println(">>>> Data" + user);
+        return "home";
     }
 }
---
+++ src/main/java/com/example/java_spring_mvc/domain/User.java
@@ -0,0 +1,68 @@
+package com.example.java_spring_mvc.domain;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String fullName;
+    private String address;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + "" +
+                "email: " + email + "" +
+                "password: " + password + "" +
+                "fullName: " + fullName + "" +
+                "address: " + address + "" +
+                "phone: " + phone + "";
+    }
+}
