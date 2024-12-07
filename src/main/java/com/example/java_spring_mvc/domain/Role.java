b7a73099f9e15e6916918585b08ad43ef400fdbb
thuanhoang-tsp <thuan.hoang@tsp.com.vn>
2024-12-07 22:26:13 +0700

    One to Many Relationship

--- src/main/java/com/example/java_spring_mvc/domain/Role.java
+++ src/main/java/com/example/java_spring_mvc/domain/Role.java
@@ -1,17 +1,25 @@
 package com.example.java_spring_mvc.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String desciption;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
 
     public long getId() {
         return id;
--- src/main/java/com/example/java_spring_mvc/domain/User.java
+++ src/main/java/com/example/java_spring_mvc/domain/User.java
@@ -1,11 +1,18 @@
 package com.example.java_spring_mvc.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +23,13 @@ public class User {
     private String address;
     private String phone;
     private String avatar;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    List<Order> orders;
 
     public String getAvatar() {
         return avatar;
